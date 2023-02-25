9d2690be567f6e245cbd6034f7637ee3ac9c6d9b
Abhishek Kumar Kamti <abhishekkamti723@gmail.com>
2023-02-25 23:42:08 +0530

    minor change

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import './Nav.css';
-import{AiOutlineHome} from "react-icons/ai";
-import{AiOutlineUser} from "react-icons/ai";
-import{BiBook} from "react-icons/bi";
-import{RiServiceLine} from "react-icons/ri";
-import{BiMessageSquareDetail} from "react-icons/bi";
-import { useState } from 'react';
+
 
 
 
 function Nav() {
-
-const [activeNav, setActiveNav] = useState("#");
-
 
   return (
     <div>Nav</div>
